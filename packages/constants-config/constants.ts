0ff5d1d46a278b5b48d98c1ae5123d897dac7690
thisisbhanuj <tobhanuj@gmail.com>
2024-07-26 12:34:28 +1000

    Added shared package for constants

---
+++ packages/constants-config/constants.ts
@@ -0,0 +1,11 @@
+const SITE_NAME = 'BHANUJ';
+const WEBSITE_URL = 'https://www.bhanuj.com';
+const CURRENCY = 'USD';
+type CurrencyType = 'USD' | 'EUR' | 'GBP';
+
+export { 
+    SITE_NAME,
+    WEBSITE_URL, 
+    CURRENCY, 
+    CurrencyType 
+};
