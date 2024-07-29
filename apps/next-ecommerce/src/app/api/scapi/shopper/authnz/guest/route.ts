c2c36ad94af1251c25e42a6d584694717d748764
thisisbhanuj <tobhanuj@gmail.com>
2024-07-29 18:05:43 +1000

    Added JSDOC for the SCAPI AUTH endpoint

--- apps/next-ecommerce/src/app/api/scapi/shopper/authnz/guest/route.ts
+++ apps/next-ecommerce/src/app/api/scapi/shopper/authnz/guest/route.ts
@@ -11,23 +11,23 @@ import { ClientConfig } from '@repo/sfcc-scapi/src/types/SCAPIType';
 
 /**
  * Serverless Function to get access token for guest user using the client credentials grant type.
- * 
+ *
  * NextJS-SCAPI Private Client Integration:
- * ****************************************************************************************
+ * *********************************************************************************************
  * For a guest user, get the shopper JWT access token and a refresh token. 
  * This is where a client appplication is able to get an access token for the guest user
  * through the back channel (a trusted server) by passing in the client credentials.
- * ****************************************************************************************
- * 
+ * *********************************************************************************************
+ *
  * OTHER CLIENT INTEGRATIONS USE CASE (Not applicable for this implementation):
- * ****************************************************************************************
+ * *********************************************************************************************
  * For a private client, an application is able to get an access token for the shopper 
  * through the back channel (a trusted server) by passing in the client credentials and
  * the authorization code retrieved from the authorize endpoint.
- * 
+ *
  * For a public client using PKCE, an application will pass a PKCE code_verifier that matches 
  * the code_challenge that was used to authorize the customer along with the authorization code.
- * ****************************************************************************************
+ * *********************************************************************************************
  * @returns The access token.
  */
 export async function POST() {
--- apps/next-ecommerce/src/hooks/useSCAPISession.tsx
+++ apps/next-ecommerce/src/hooks/useSCAPISession.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { shopperGuestAccessToken } from '../sfcc/orchestration/SCAPIShopper';
 import { ShopperSessionSCAPI } from '@repo/types-config/CommonTypes';
 
@@ -9,28 +9,43 @@ export const useSCAPISession = () => {
     null,
   );
 
-  useEffect(() => {
-    const fetchSessionData = async () => {
-      let sfccSessionData: ShopperSessionSCAPI | null = null;
-      const storedSessionData = localStorage.getItem('sfccSessionData');
-      // Fetch and store the session data in the local storage
-      if (storedSessionData) {
-        sfccSessionData = JSON.parse(storedSessionData);
-      } else {
-        sfccSessionData = await shopperGuestAccessToken();
-        localStorage.setItem(
-          'sfccSessionData',
-          JSON.stringify(sfccSessionData),
-        );
-      }
-      // Store the session data in the cookie
-      if (sfccSessionData) {
-        setSessionData(sfccSessionData);
-        document.cookie = `sfccSessionData=${JSON.stringify(sfccSessionData)}; path=/;`;
-      }
-    };
+  /**
+   * Using useRef can help prevent certain code within a useEffect from running multiple times 
+   * by tracking whether the effect has already been run. This is especially useful in development
+   * with React's Strict Mode, where effects might be run twice for the purposes of highlighting side effects.
+   * 
+   * Here's how useRef helps in detail:
+   * useRef is a hook that allows you to persist values across renders without causing re-renders when the value changes.
+   * It returns a mutable ref object whose .current property can be set to any value.
+   * Unlike state, updating a ref does not trigger a re-render.
+   */
+  const effectRan = useRef(false);
 
-    fetchSessionData();
+  useEffect(() => {
+    if (effectRan.current === false) {
+      const fetchSessionData = async () => {
+        let sfccSessionData: ShopperSessionSCAPI | null = null;
+        const storedSessionData = sessionStorage.getItem('sfccSessionData');
+
+        if (storedSessionData) {
+          sfccSessionData = JSON.parse(storedSessionData);
+        } else {
+          sfccSessionData = await shopperGuestAccessToken();
+          sessionStorage.setItem(
+            'sfccSessionData',
+            JSON.stringify(sfccSessionData),
+          );
+        }
+
+        if (sfccSessionData) {
+          setSessionData(sfccSessionData);
+          document.cookie = `sfccSessionData=${JSON.stringify(sfccSessionData)}; path=/;`;
+        }
+      };
+
+      fetchSessionData();
+      effectRan.current = true;
+    }
   }, []);
 
   return sessionData;
